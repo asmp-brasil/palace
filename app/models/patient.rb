756576ba1c5d9d14159002dad0f612e9e7f55649
emethium <nikolas.serafini@gmail.com>
2020-04-19 17:04:57 -0300

    feat: added patient validations and status concern

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,2 +1,15 @@
+# frozen_string_literal: true
+
+# Patient entity model
 class Patient < ApplicationRecord
+  include HasStatus
+
+  before_create :set_default_status
+  validates :name, :crm, :phone, :email, presence: true
+
+  enum
+
+  def set_default_status
+    self.status = HasStatus.status.pending
+  end
 end
