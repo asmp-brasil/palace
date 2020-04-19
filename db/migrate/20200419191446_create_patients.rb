29de20324a8c78658562213e4806f69be9765ba4
emethium <nikolas.serafini@gmail.com>
2020-04-19 16:18:24 -0300

    feat: generated patient model

---
+++ db/migrate/20200419191446_create_patients.rb
@@ -0,0 +1,22 @@
+class CreatePatients < ActiveRecord::Migration[6.0]
+  def change
+    create_table :patients do |t|
+      t.string :name
+      t.string :sex
+      t.string :civil_state
+      t.integer :children
+      t.text :drug_usage
+      t.string :previous_treatment
+      t.string :cirurgies
+      t.string :comorbidities
+      t.string :profession
+      t.string :workplace
+      t.string :crm
+      t.string :status
+      t.string :phone
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
