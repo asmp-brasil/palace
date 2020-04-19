958dd98a579445db244a5c08aa5907e703b38c03
emethium <nikolas.serafini@gmail.com>
2020-04-19 18:55:38 -0300

    fix: patient creation woking

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,14 +2,10 @@
 
 # Patient entity model
 class Patient < ApplicationRecord
-  include HasStatus
-
   before_create :set_default_status
   validates :name, :crm, :phone, :email, presence: true
 
-  enum
-
   def set_default_status
-    self.status = HasStatus.status.pending
+    self.status = 'pending'
   end
 end
