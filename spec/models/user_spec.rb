150617a002ef5a53e0046961a981883bc3d6e8ba
Igor Sant'ana <igorba2005@gmail.com>
2020-04-20 21:33:11 -0300

    feat: add user model and devise

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  let(:user) { FactoryBot.build :user }
+  it 'validates' do
+    expect(user).to be_valid
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,7 @@ SimpleCov.start 'rails' do
 end
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'support/factory_bot'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
