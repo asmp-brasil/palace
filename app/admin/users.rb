9c634b9164abb85248746fa7fba944ad83af68ec
Igor Sant'ana <igorba2005@gmail.com>
2020-04-21 23:12:38 -0300

    lint: some rubocop issues

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 ActiveAdmin.register User do
   permit_params :email, :password
 
@@ -30,7 +32,7 @@ ActiveAdmin.register User do
     f.inputs do
       f.input :email
       f.input :password
-     end
+    end
     f.actions
   end
 end
