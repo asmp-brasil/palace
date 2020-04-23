8ddecea30435060999128e425b1a2259d288f452
Igor Sant'ana <igorba2005@gmail.com>
2020-04-22 22:33:39 -0300

    feat: add current models translations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  def set_admin_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options(options={})
+    { :locale => I18n.locale }
+  end
 end
