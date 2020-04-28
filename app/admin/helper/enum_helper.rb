cec4dd20bb077da392e1c648ea9e27b4151e6a43
emethium <nikolas.serafini@gmail.com>
2020-04-27 22:22:48 -0300

    chore: linting errors

--- app/admin/helper/enum_helper.rb
+++ app/admin/helper/enum_helper.rb
@@ -3,6 +3,6 @@
 # Helper of enum select fields
 module EnumHelper
   def self.translated_options(enum, property_name)
-    enum.map { |key,value| [I18n.t("activerecord.attributes.shared.#{property_name}.#{key}"), value] }
+    enum.map { |key, value| [I18n.t("activerecord.attributes.shared.#{property_name}.#{key}"), value] }
   end
 end
