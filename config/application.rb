f4256c3cafa8306415ee19022960393fcaa888ba
Igor Sant'ana <igorba2005@gmail.com>
2020-05-24 16:01:09 -0300

    fix: disabling unnecessary rules for rubocop
    co-authored-by: Nikolas Serafini <nikolas.serafini@gmail.com>

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module App
-  class Application < Rails::Application # rubocop:todo Style/Documentation
+  class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.i18n.default_locale = 'pt-BR'
